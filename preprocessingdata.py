3ee7251190c9ad65f67a045b7252eb168671a961
AKashton <ashtonrcurry@hotmail.com>
2023-12-04 19:45:28 -0900

    added classifer for training

--- preprocessingdata.py
+++ preprocessingdata.py
@@ -71,3 +71,26 @@ tfidf_features.shape
 #split data into training and test sets
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(tfidf_features, tweets_labels, test_size=0.2, random_state=42)
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Initialize the Naive Bayes Classifier
+clf = MultinomialNB()
+
+# Fit the classifier to the training data
+clf.fit(X_train, y_train)
+
+# Predict on the test data
+y_pred = clf.predict(X_test)
+
+# Calculate and print metrics
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
