9a452c67e83980b492cc95d2496390c2140eade5
AKashton <ashtonrcurry@hotmail.com>
2023-12-04 20:31:04 -0900

    added ROC code

--- Trollhunter.py
+++ Trollhunter.py
@@ -111,3 +111,26 @@ plt.title('Confusion Matrix')
 plt.ylabel('Actual Label')
 plt.xlabel('Predicted Label')
 plt.show()
+
+#plotting ROC graph
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+# Compute probabilities
+y_probs = clf.predict_proba(X_test)[:, 1]  # Probabilities for the positive class
+
+# Compute ROC curve and ROC area
+fpr, tpr, _ = roc_curve(y_test, y_probs)
+roc_auc = auc(fpr, tpr)
+
+# Plotting the ROC curve
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
