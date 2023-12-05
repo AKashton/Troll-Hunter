83a8c8ba6e707a94bca422eb485dd64fc4ba0c73
AKashton <ashtonrcurry@hotmail.com>
2023-12-04 19:10:08 -0900

    added requirements.txt and preprocessing code

--- preprocessingdata.py
+++ preprocessingdata.py
@@ -1,4 +1,10 @@
-import json
+import json, re, nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 
 # Path to the JSON file
 file_path = 'Dataset for Detection of Cyber-Trolls.json'
@@ -10,3 +16,37 @@ with open(file_path, 'r') as file:
 
 # Convert the JSON strings to dictionaries and store them in a list
 tweets = [json.loads(line) for line in data]
+
+# Download necessary NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Initialize the Porter Stemmer (will reduce words to base form)
+stemmer = PorterStemmer()
+
+# Extracting content and labels (gets text from tweet and lable for troll or non troll)
+tweets_content = [tweet['content'] for tweet in tweets]
+tweets_labels = [int(tweet['annotation']['label'][0]) for tweet in tweets]
+
+# Define a function for cleaning text
+def clean_text(text):
+    # Convert text to lowercase
+    text = text.lower()
+    # Remove URLs
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
+    # Remove user @ references and '#' from tweet
+    text = re.sub(r'\@\w+|\#','', text)
+    # Remove punctuations
+    text = re.sub(r'[^\w\s]', '', text)
+    # Tokenization
+    tokenized_text = word_tokenize(text)
+    # Remove stopwords and perform stemming
+    cleaned_text = [stemmer.stem(word) for word in tokenized_text if word not in stopwords.words('english')]
+    
+    return ' '.join(cleaned_text)
+
+# Clean the tweets
+cleaned_tweets = [clean_text(tweet) for tweet in tweets_content]
+
+# Display the first few cleaned tweets
+cleaned_tweets[:5], tweets_labels[:5]
